06e1ab4891190ca28cb8446103a3f56b042336f6
Shiwangi Kumari <sshiwangi770@gmail.com>
2023-03-06 14:25:03 +0530

    updating app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
     <div className="App">
       <Router>
         <Navbar />
-        <Home />
         <Routes>
           <Route path='/' element={<Home />} />
         </Routes>
