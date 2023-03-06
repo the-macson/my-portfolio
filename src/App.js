6c3f2438fe50b2166e0aff8a7af3dedd4ca25752
Shiwangi Kumari <sshiwangi770@gmail.com>
2023-03-06 12:12:13 +0530

    updating app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home';
+import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
       <Router>
         <Navbar />
+        <Home />
         <Routes>
           <Route path='/' element={<Home />} />
         </Routes>
