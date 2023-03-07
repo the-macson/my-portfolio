a0a4ef69c1a57cea4baf459e7ebff5a802ca5e51
Shiwangi Kumari <sshiwangi770@gmail.com>
2023-03-07 23:21:10 +0530

    replaced homepage url with gh page url

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
 
 function App() {
   return (
